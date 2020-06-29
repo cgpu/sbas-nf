b190452a02456ac0015160870454c921b1cc12b1
cgpu <chatzipantsiou@gmail.com>
2020-06-29 12:21:56 +0000

    Adds explicit version for all libs; Migrates R utils in env.yml

--- containers/papermill-sbas/install.R
+++ containers/papermill-sbas/install.R
@@ -1,2 +1,2 @@
 Sys.setenv(TAR = "/bin/tar")
-install.packages("runjags", repos = "https://cloud.r-project.org/")
+install.packages("runjags", version = "2.0.4-6", repos = "https://cloud.r-project.org/")
