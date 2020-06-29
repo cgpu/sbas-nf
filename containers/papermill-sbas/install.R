e0b6387e8ce43ff896e87eae99c031acab7da1e5
EC2 Default User <ec2-user@ip-172-31-27-71.eu-west-1.compute.internal>
2020-06-29 11:14:42 +0000

    removes piggyback, repr from install.R

--- containers/papermill-sbas/install.R
+++ containers/papermill-sbas/install.R
@@ -1,5 +1,2 @@
 Sys.setenv(TAR = "/bin/tar")
-devtools::install_github("ropensci/piggyback@87f71e8")
-# https://github.com/cgpu/sbas-nf/issues/4
-devtools::install_github("IRkernel/repr@505a052")
 install.packages("runjags", repos = "https://cloud.r-project.org/")
